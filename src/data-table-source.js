257224f3b540113b48861f829c066ae9b34cebf9
Jannik Günther <jannik.guenther@lgln.niedersachsen.de>
2022-07-01 15:00:25 +0200

    added tests and refactoring

--- src/data-table-source.js
+++ src/data-table-source.js
@@ -1,20 +1,76 @@
 module.exports = class DataTableSource {
+
+    #data
+    #renderedData
+    #filteredData
+    #filter = ''
+
     constructor(data) {
+        if (!Array.isArray(data)) {
+            throw new Error("Can not create DataTableSource. Input data needs to be an array of objects.")
+        }
+        if (data.length > 0) {
+            if (!this._checkIfArrayConsistsOfObjects(data)) {
+                throw new Error("Can not create DataTableSource. Input data needs to be an array of objects.")
+            }
+            if (!this._checkIfObjectsHaveSameKeys(data)) {
+                throw new Error("Objects in data array do not have uniform keys")
+            }
+        }
+
         this.data = data
-        this._filteredData = this.data
     }
 
-    get filteredData(){
-        return this._filteredData
+    set data(value) {
+        this.#data = value
+        this._render()
     }
 
-    filter(filter) {
-        this._filteredData = this.data.filter(dataObject => {
+    get filter(){
+        return this.#filter
+    }
+
+    set filter(value){
+        this.#filter = value
+        this._render()
+    }
+
+    get renderedData(){
+        return this.#renderedData
+    }
+
+    _checkIfArrayConsistsOfObjects(data) {
+        return !data.some((currentValue) => {
+            return typeof currentValue !== 'object' || Array.isArray(currentValue) || !currentValue
+        })
+    }
+
+    _checkIfObjectsHaveSameKeys(data) {
+        const firstObject = data[0]
+        return data.every((currentValue) => {
+            return JSON.stringify(Object.keys(firstObject).sort()) === JSON.stringify(Object.keys(currentValue).sort());
+        })
+    }
+
+    _filterData(filter) {
+        if (typeof filter === 'number') {
+            filter = filter.toString()
+        }
+        this.#filteredData = this.#data.filter(dataObject => {
             const dataStr = Object.keys(dataObject).reduce((currentTerm, key) => {
                 return currentTerm + dataObject[key] + '◬';
             }, '').toLowerCase()
 
             return dataStr.indexOf(filter.toLowerCase().trim()) !== -1
         })
+    }
+
+    _render() {
+        this._filterData(this.filter)
+        this.#renderedData = this.#filteredData
+    }
+
+    resetFilter() {
+        this.filter = ''
     }
 }
--- test/data-table-source.test.js
+++ test/data-table-source.test.js
@@ -2,33 +2,114 @@ const chai = require('chai')
 const DataTableSource = require("../src/data-table-source");
 
 const expect = chai.expect;
-describe('DataTableSourceTest', function () {
 
-    let data;
+let data;
 
-    beforeEach(() => {
-        data = [
-            {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-            {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-            {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-            {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-            {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-            {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-            {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-            {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-            {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-            {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-        ];
-    })
+before(() => {
+    data = [
+        {name: 'Dragon Rider', album: 'Archangel', year: 2010, length: 1.53, composer: 'Thomas Bergersen'},
+        {name: 'Fire Nation', album: 'Invincible', year: 2010, length: 2.59, composer: 'Nick Phoenix'},
+        {name: 'Blackheart', album: 'SkyWorld', year: 2012, length: 4.32, composer: 'Thomas Bergersen'},
+        {name: 'Empire of Angles', album: 'Sun', year: 2015, length: 5.16, composer: 'Thomas Bergersen'},
+        {name: 'Impossible', album: 'Unleashed', year: 2017, length: 8.54, composer: 'Thomas Bergersen'},
+        {name: 'Battleborne', album: 'Battlecry', year: 2015, length: 5.08, composer: 'Nick Phoenix'},
+        {name: 'Orion', album: 'Orion', year: 2019, length: 8.07, composer: 'Michal Cielecki'},
+        {name: 'Shield of Love', album: 'Songs for Ukraine', year: 2022, length: 3.03, composer: 'Thomas Bergersen'},
+        {
+            name: 'One Million Voices',
+            album: 'Humanity - Chapter IV',
+            year: 2021,
+            length: 8.53,
+            composer: 'Thomas Bergersen'
+        },
+        {name: 'Nightwood', album: 'Colin Frake On Fire Mountain', year: 2014, length: 3.09, composer: 'Nick Phoenix'},
+    ];
+})
 
-    it('should create a DataTableSourceTest', function () {
+describe('DataTableSource constructor tests', function () {
+
+    it('should create a DataTableSource', function () {
         const dataTableSource = new DataTableSource(data)
         expect(dataTableSource).to.be.instanceof(DataTableSource)
     });
 
+    it('should throw an error when Input Objects have different keys', function () {
+        expect(() => new DataTableSource([{
+            name: 'Dragon Rider',
+            album: 'Archangel',
+            year: 2010,
+            length: 1.53,
+            composer: 'Thomas Bergersen'
+        },
+            {name: 'Fire Nation', album: 'Invincible', year: 2010, length: 2.59, composer: 'Nick Phoenix'},
+            {name: 'Blackheart', album: 'SkyWorld', year: 2012, length: 4.32, singer: 'Merethe Soltvedt'}])
+        ).to.throw('Objects in data array do not have uniform keys')
+    });
+
+    it('should not create DataTableSource when input is no array', function () {
+        expect(() => new DataTableSource(1337)
+        ).to.throw('Can not create DataTableSource. Input data needs to be an array of objects.')
+    });
+
+    it('should not create DataTableSource when input is no array of objects', function () {
+        expect(() => new DataTableSource([1337, 1234, 4321])
+        ).to.throw('Can not create DataTableSource. Input data needs to be an array of objects.')
+    });
+})
+
+
+describe('DataTableSource filter tests', function () {
+
     it('should filter data by string', function () {
         const dataTableSource = new DataTableSource(data)
-        dataTableSource.filter("hi")
-        expect(dataTableSource.filteredData).eql([{position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'}])
+        dataTableSource.filter = "Cie"
+        expect(dataTableSource.renderedData).eql([{
+            name: 'Orion',
+            album: 'Orion',
+            year: 2019,
+            length: 8.07,
+            composer: 'Michal Cielecki'
+        }])
     });
+
+    it('should not be case-sensitive', function () {
+        const dataTableSource = new DataTableSource(data)
+        dataTableSource.filter = "bAtTlEbOrNe"
+        expect(dataTableSource.renderedData).eql([{
+            name: 'Battleborne',
+            album: 'Battlecry',
+            year: 2015,
+            length: 5.08,
+            composer: 'Nick Phoenix'
+        }])
+    });
+
+    it('should filter data by int number', function () {
+        const dataTableSource = new DataTableSource(data)
+        dataTableSource.filter = 2010
+        expect(dataTableSource.renderedData).eql(
+            [
+                {name: 'Dragon Rider', album: 'Archangel', year: 2010, length: 1.53, composer: 'Thomas Bergersen'},
+                {name: 'Fire Nation', album: 'Invincible', year: 2010, length: 2.59, composer: 'Nick Phoenix'}
+            ]
+        )
+    });
+
+    it('should filter data by float number', function () {
+        const dataTableSource = new DataTableSource(data)
+        dataTableSource.filter = 4.32
+        expect(dataTableSource.renderedData).eql(
+            [
+                {name: 'Blackheart', album: 'SkyWorld', year: 2012, length: 4.32, composer: 'Thomas Bergersen'}
+            ]
+        )
+    });
+
+    it('should reset filter', function () {
+        const dataTableSource = new DataTableSource(data)
+        dataTableSource.filter = 'Bergersen'
+        dataTableSource.resetFilter()
+        expect(dataTableSource.renderedData).eql(data)
+    });
+
 });
