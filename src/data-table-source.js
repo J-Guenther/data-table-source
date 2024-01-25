e1a66259c8abcac43c9d1da559dca59d882fac01
J-Guenther <jannik.guentheg@gmail.com>
2024-01-25 20:17:36 +0100

    added pagination

--- src/data-table-source.js
+++ src/data-table-source.js
@@ -4,55 +4,101 @@ module.exports = class DataTableSource {
     #renderedData
     #filteredData
     #filter = ''
+    #pageSize
+    #currentPage = 1
 
-    constructor(data) {
+    constructor(data, pageSize = 0) {
         if (!Array.isArray(data)) {
             throw new Error("Can not create DataTableSource. Input data needs to be an array of objects.")
         }
         if (data.length > 0) {
-            if (!this._checkIfArrayConsistsOfObjects(data)) {
+            if (!this.#checkIfArrayConsistsOfObjects(data)) {
                 throw new Error("Can not create DataTableSource. Input data needs to be an array of objects.")
             }
-            if (!this._checkIfObjectsHaveSameKeys(data)) {
+            if (!this.#checkIfObjectsHaveSameKeys(data)) {
                 throw new Error("Objects in data array do not have uniform keys")
             }
         }
 
         this.data = data
+        this.pageSize = pageSize
     }
 
     set data(value) {
         this.#data = value
-        this._render()
+        this.#render()
     }
 
-    get filter(){
+    get filter() {
         return this.#filter
     }
 
-    set filter(value){
+    set filter(value) {
+        this.#currentPage = 1
         this.#filter = value
-        this._render()
+        this.#render()
     }
 
-    get renderedData(){
+    get renderedData() {
         return this.#renderedData
     }
 
-    _checkIfArrayConsistsOfObjects(data) {
+    set pageSize(value) {
+        if (typeof value === 'number' && value >= 0) {
+            this.#pageSize = value
+            this.#render()
+        } else {
+            throw new Error('pageSize must be a number greater or equal 0')
+        }
+    }
+
+    get pageSize() {
+        return this.#pageSize
+    }
+
+    set currentPage(value) {
+        if (typeof value === 'number' && value > 0) {
+            this.#currentPage = value
+            this.#render()
+        } else {
+            throw new Error('currentPage must be a positive number')
+        }
+    }
+
+    get currentPage() {
+        return this.#currentPage
+    }
+
+    getMaxPages() {
+        return this.#pageSize > 0 ? Math.ceil(this.#filteredData.length / this.#pageSize) : 1
+    }
+
+    nextPage() {
+        if (this.#currentPage < this.getMaxPages()) {
+            this.currentPage += 1;
+        }
+    }
+
+    previousPage() {
+        if (this.#currentPage > 1) {
+            this.currentPage -= 1;
+        }
+    }
+
+    #checkIfArrayConsistsOfObjects(data) {
         return !data.some((currentValue) => {
             return typeof currentValue !== 'object' || Array.isArray(currentValue) || !currentValue
         })
     }
 
-    _checkIfObjectsHaveSameKeys(data) {
+    #checkIfObjectsHaveSameKeys(data) {
         const firstObject = data[0]
         return data.every((currentValue) => {
             return JSON.stringify(Object.keys(firstObject).sort()) === JSON.stringify(Object.keys(currentValue).sort());
         })
     }
 
-    _filterData(filter) {
+    #filterData(filter) {
         if (typeof filter === 'number') {
             filter = filter.toString()
         }
@@ -65,9 +111,16 @@ module.exports = class DataTableSource {
         })
     }
 
-    _render() {
-        this._filterData(this.filter)
-        this.#renderedData = this.#filteredData
+    #render() {
+        this.#filterData(this.filter)
+
+        const startIndex = (this.#currentPage - 1) * this.#pageSize;
+        let endIndex = startIndex + this.#pageSize;
+        if (endIndex <= 0) {
+            endIndex = this.#filteredData.length;
+        }
+
+        this.#renderedData = this.#filteredData.slice(startIndex, endIndex);
     }
 
     resetFilter() {
--- test/data-table-source.test.js
+++ test/data-table-source.test.js
@@ -113,3 +113,136 @@ describe('DataTableSource filter tests', function () {
     });
 
 });
+
+
+describe('DataTableSource pagination tests', function () {
+
+    it('should display data with page size 5', function () {
+        const dataTableSource = new DataTableSource(data,  5)
+        expect(dataTableSource.renderedData).eql([{name: 'Dragon Rider', album: 'Archangel', year: 2010, length: 1.53, composer: 'Thomas Bergersen'},
+            {name: 'Fire Nation', album: 'Invincible', year: 2010, length: 2.59, composer: 'Nick Phoenix'},
+            {name: 'Blackheart', album: 'SkyWorld', year: 2012, length: 4.32, composer: 'Thomas Bergersen'},
+            {name: 'Empire of Angles', album: 'Sun', year: 2015, length: 5.16, composer: 'Thomas Bergersen'},
+            {name: 'Impossible', album: 'Unleashed', year: 2017, length: 8.54, composer: 'Thomas Bergersen'}])
+    });
+
+    it('should get pageSize', function () {
+        const dataTableSource = new DataTableSource(data,  1337)
+        expect(dataTableSource.pageSize).eql(1337)
+    });
+
+    it('should initialize currentPage to 1', function () {
+        const dataTableSource = new DataTableSource(data,  1337)
+        expect(dataTableSource.currentPage).eql(1)
+    });
+
+    it('should display all data if pageSize is 0', function () {
+        const dataTableSource = new DataTableSource(data,  0)
+        expect(dataTableSource.renderedData).eql(data)
+    });
+
+    it('should throw an error if set pageSize is invalid', function () {
+        const dataTableSource = new DataTableSource(data)
+        expect(() => dataTableSource.pageSize = -1
+        ).to.throw('pageSize must be a number greater or equal 0')
+
+        expect(() => dataTableSource.pageSize = 'Trondheim'
+        ).to.throw('pageSize must be a number greater or equal 0')
+    });
+
+    it('should display the second page', function () {
+        const dataTableSource = new DataTableSource(data,  5)
+        dataTableSource.currentPage = 2
+        expect(dataTableSource.renderedData).eql([{name: 'Battleborne', album: 'Battlecry', year: 2015, length: 5.08, composer: 'Nick Phoenix'},
+            {name: 'Orion', album: 'Orion', year: 2019, length: 8.07, composer: 'Michal Cielecki'},
+            {name: 'Shield of Love', album: 'Songs for Ukraine', year: 2022, length: 3.03, composer: 'Thomas Bergersen'},
+            {
+                name: 'One Million Voices',
+                album: 'Humanity - Chapter IV',
+                year: 2021,
+                length: 8.53,
+                composer: 'Thomas Bergersen'
+            },
+            {name: 'Nightwood', album: 'Colin Frake On Fire Mountain', year: 2014, length: 3.09, composer: 'Nick Phoenix'}])
+    });
+
+    it('should throw an error if currentPage is invalid', function () {
+        const dataTableSource = new DataTableSource(data)
+        expect(() => dataTableSource.currentPage = -1
+        ).to.throw('currentPage must be a positive number')
+
+        expect(() => dataTableSource.currentPage = 0
+        ).to.throw('currentPage must be a positive number')
+
+        expect(() => dataTableSource.currentPage = 'Trondheim'
+        ).to.throw('currentPage must be a positive number')
+    });
+
+    it('should get the correct maxPageSize', function () {
+        const dataTableSource = new DataTableSource(data,  3)
+        expect(dataTableSource.getMaxPages()).eql(4)
+        dataTableSource.pageSize = 0
+        expect(dataTableSource.getMaxPages()).eql(1)
+    });
+
+    it('should go to the next page', function () {
+        const dataTableSource = new DataTableSource(data,  3)
+        dataTableSource.nextPage()
+        dataTableSource.nextPage()
+        dataTableSource.nextPage()
+        expect(dataTableSource.currentPage).eql(4)
+        expect(dataTableSource.renderedData).eql([{name: 'Nightwood', album: 'Colin Frake On Fire Mountain', year: 2014, length: 3.09, composer: 'Nick Phoenix'}])
+    });
+
+    it('should go to the first page', function () {
+        const dataTableSource = new DataTableSource(data,  3)
+        dataTableSource.currentPage = 4
+        dataTableSource.previousPage()
+        dataTableSource.previousPage()
+        dataTableSource.previousPage()
+        expect(dataTableSource.currentPage).eql(1)
+        expect(dataTableSource.renderedData).eql([ {name: 'Dragon Rider', album: 'Archangel', year: 2010, length: 1.53, composer: 'Thomas Bergersen'},
+            {name: 'Fire Nation', album: 'Invincible', year: 2010, length: 2.59, composer: 'Nick Phoenix'},
+            {name: 'Blackheart', album: 'SkyWorld', year: 2012, length: 4.32, composer: 'Thomas Bergersen'}])
+    });
+
+    it('should not go exceed maxPage when calling nextPage()', function () {
+        const dataTableSource = new DataTableSource(data,  5)
+        dataTableSource.nextPage()
+        dataTableSource.nextPage()
+        dataTableSource.nextPage()
+        expect(dataTableSource.currentPage).eql(dataTableSource.getMaxPages())
+    });
+
+    it('should not go below 1 when calling previousPage()', function () {
+        const dataTableSource = new DataTableSource(data,  5)
+        dataTableSource.previousPage()
+        expect(dataTableSource.currentPage).eql(1)
+    });
+
+    it('should reset Pagination when filter is used', function () {
+        const dataTableSource = new DataTableSource(data,  3)
+        dataTableSource.nextPage()
+        dataTableSource.nextPage()
+        expect(dataTableSource.currentPage).eql(3)
+        dataTableSource.filter = 'Nightwood'
+        expect(dataTableSource.currentPage).eql(1)
+        expect(dataTableSource.getMaxPages()).eql(1)
+        expect(dataTableSource.renderedData).eql([{name: 'Nightwood', album: 'Colin Frake On Fire Mountain', year: 2014, length: 3.09, composer: 'Nick Phoenix'}])
+    });
+
+    it('should reset Pagination when filter is used and filter is reset', function () {
+        const dataTableSource = new DataTableSource(data,  3)
+        dataTableSource.nextPage()
+        dataTableSource.nextPage()
+        expect(dataTableSource.currentPage).eql(3)
+        dataTableSource.filter = 'thomas'
+        expect(dataTableSource.currentPage).eql(1)
+        expect(dataTableSource.getMaxPages()).eql(2)
+        dataTableSource.nextPage()
+        expect(dataTableSource.currentPage).eql(2)
+        dataTableSource.resetFilter()
+        expect(dataTableSource.currentPage).eql(1)
+        expect(dataTableSource.getMaxPages()).eql(4)
+    });
+});
